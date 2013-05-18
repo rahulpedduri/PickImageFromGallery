7814251573379b130f544728fb6b18476845c08e
karthik <karthik.trn@gmail.com>
2013-05-18 00:07:55 -0400

    Image picked and displayed in gallery
    Image picked from gallery and displayed on Welcome Activity(for testing)

--- gen/com/trnkarthik/pickimagefromgallery/R.java
+++ gen/com/trnkarthik/pickimagefromgallery/R.java
@@ -18,11 +18,13 @@ public final class R {
         public static final int rounded_corners_white=0x7f020004;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070004;
-        public static final int sphinxLogoScreenApp=0x7f070003;
-        public static final int sphinxLogoScreenTagLine=0x7f070001;
-        public static final int sphinxLogoScreenTitle=0x7f070002;
-        public static final int splashScreenLayout=0x7f070000;
+        public static final int gallerySelectButton=0x7f070000;
+        public static final int imageView1=0x7f070001;
+        public static final int menu_settings=0x7f070006;
+        public static final int sphinxLogoScreenApp=0x7f070005;
+        public static final int sphinxLogoScreenTagLine=0x7f070003;
+        public static final int sphinxLogoScreenTitle=0x7f070004;
+        public static final int splashScreenLayout=0x7f070002;
     }
     public static final class layout {
         public static final int activity_welcome=0x7f030000;
@@ -33,6 +35,7 @@ public final class R {
         public static final int activity_welcome_splash=0x7f060001;
     }
     public static final class string {
+        public static final int GallerySelect=0x7f040007;
         public static final int app_name=0x7f040000;
         public static final int applications=0x7f040004;
         public static final int hello_world=0x7f040001;
--- src/com/trnkarthik/pickimagefromgallery/WelcomeActivity.java
+++ src/com/trnkarthik/pickimagefromgallery/WelcomeActivity.java
@@ -1,15 +1,42 @@
 package com.trnkarthik.pickimagefromgallery;
 
-import android.os.Bundle;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class WelcomeActivity extends Activity {
 
+	private static final int SELECT_PHOTO = 100;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome);
+		
+		Button gallerySelect = (Button)findViewById(R.id.gallerySelectButton);
+		gallerySelect.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+
+				Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+				photoPickerIntent.setType("image/*");
+				startActivityForResult(photoPickerIntent, SELECT_PHOTO);    
+
+				
+			}
+		});
+		
 	}
 
 	@Override
@@ -17,6 +44,28 @@ public class WelcomeActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_welcome, menu);
 		return true;
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) { 
+	    super.onActivityResult(requestCode, resultCode, imageReturnedIntent); 
+
+	    switch(requestCode) { 
+	    case SELECT_PHOTO:
+	        if(resultCode == RESULT_OK){  
+	            Uri selectedImage = imageReturnedIntent.getData();
+	            InputStream imageStream = null;
+				try {
+					imageStream = getContentResolver().openInputStream(selectedImage);
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	            Bitmap yourSelectedImage = BitmapFactory.decodeStream(imageStream);
+	            ImageView image = (ImageView) findViewById(R.id.imageView1);
+	            image.setImageBitmap(yourSelectedImage);
+	        }
+	    }
 	}
 
 }
