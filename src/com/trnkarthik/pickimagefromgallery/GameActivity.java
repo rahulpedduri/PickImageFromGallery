222d7e048722227d86838c65eca87e98f050385c
karthik <karthik.trn@gmail.com>
2013-05-18 03:11:39 -0400

    Level,image passed to GameActivity(error-see desc)
    Passed level and image to Game Activity.
    Encountered FAILED BINDER TRANSACTION error.

--- src/com/trnkarthik/pickimagefromgallery/GameActivity.java
+++ src/com/trnkarthik/pickimagefromgallery/GameActivity.java
@@ -1,15 +1,32 @@
 package com.trnkarthik.pickimagefromgallery;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class GameActivity extends Activity {
 
+	public Bitmap bitmap;
+	public Integer level;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
+		
+		bitmap = (Bitmap) getIntent().getExtras().get("Image");
+		level = (Integer) getIntent().getExtras().get("level");
+		
+//		Toast.makeText(getApplicationContext(), level+"", Toast.LENGTH_LONG).show();
+
+		ImageView testImage = (ImageView)findViewById(R.id.gameImageTest);
+		testImage.setImageBitmap(bitmap);
+		
+		TextView testTextView = (TextView)findViewById(R.id.LevelTest);
+		testTextView.setText("Level : "+ level);
+		
 	}
 
 	@Override
--- src/com/trnkarthik/pickimagefromgallery/LevelSelectorActivity.java
+++ src/com/trnkarthik/pickimagefromgallery/LevelSelectorActivity.java
@@ -4,22 +4,63 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class LevelSelectorActivity extends Activity {
 
+	public RadioGroup LevelRadio;
+	public Bitmap bitmap;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_level_selector);
 		
-		Intent intent = getIntent();
-		Bitmap bitmap = (Bitmap) getIntent().getExtras().get("Image");
+		bitmap = (Bitmap) getIntent().getExtras().get("Image");
 
 		ImageView ShowCaseImage = (ImageView)findViewById(R.id.showCaseImage);
 		ShowCaseImage.setImageBitmap(bitmap);
 
+		Button toGameButton = (Button)findViewById(R.id.toGameButton);
+		toGameButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				LevelRadio =(RadioGroup) findViewById(R.id.radioGroup1);
+				String selectedRadioValue = ((RadioButton)findViewById(LevelRadio.getCheckedRadioButtonId() )).getText().toString();
+				
+				int level = 0;
+				
+				Log.d("test", selectedRadioValue+"");
+				
+				if(selectedRadioValue.equals("Play 3*3 level"))
+				{
+					level = 3;
+				}
+				else if(selectedRadioValue.equals("Play 4*4 level"))
+				{
+					level = 4;
+				}
+				
+				Toast.makeText(getApplicationContext(), level +"", Toast.LENGTH_LONG).show();
+				
+				Intent toMainGame = new Intent(LevelSelectorActivity.this, GameActivity.class);
+				toMainGame.putExtra("level", level);
+				toMainGame.putExtra("Image",bitmap);
+				startActivity(toMainGame);
+				
+			}
+		});
+		
 	}
 
 	@Override
